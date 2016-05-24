9b379073a8275cfaff0aeadf27505aca6c3733e9
Mauricio Arruda Cruz <mauacruz@gmail.com>
2016-05-23 21:07:17 -0300

    Commit Inicial- CodePress 23/05/2016

---
+++ CodePress/packages/codecategory/src/CodeCategory/Providers/CodeCategoryServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CodePress\CodeCategory\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class CodeCategoryServiceProvider extends ServiceProvider
+{
+    public function boot(){
+        $this->publishes([__DIR__ . '/../../resources/migrations/'=> base_path('database/migrations')],'migrations');
+    }
+    public function register(){
+
+}
+
+}
